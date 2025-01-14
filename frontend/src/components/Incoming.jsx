5a0ac153ae4719d832b189f562412ccaa1cfcc1a
Gautam Aggarwal-05 <aggarwalgautam06@gmail.com>
2025-01-14 11:31:26 +0530

    incoming/outgoing

--- frontend/src/components/Incoming.jsx
+++ frontend/src/components/Incoming.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Incoming = () => {
   return (
-    <div>Incoming</div>
+    <div className='max-w-[70%] bg-[#1d2437] p-2 rounded-e-2xl rounded-tl-2xl self-start '>
+      <p className='text-white text-left break-words overflow-hidden '>
+      blah blah blah blah
+       blah blah blah blah blah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blah
+        hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+        </p>
+    </div>
   )
 }
 
--- frontend/src/components/Outgoing.jsx
+++ frontend/src/components/Outgoing.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Outgoing = () => {
   return (
-    <div>Outgoing</div>
+    <div className='max-w-[70%] bg-[#dd1d5d80] p-2 rounded-s-2xl rounded-tr-2xl self-end text-white text-right flex flex-col'>
+      <p className='text-white text-left break-words overflow-hidden relative'>
+      blah blah blah blah
+       blah blah blah blah blah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blah
+        hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+        </p>
+    </div>
   )
 }
 
